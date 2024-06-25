d03dc7fdfd593318ecf80c633082c2d87b1b5e39
Yangdaehan <yangdh08@naver.com>
2024-06-25 15:12:06 +0900

    Initial commit

---
+++ src/main/java/org/sopt/gptapi/GptApiApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.gptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt/gptapi/GptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.gptapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
