68bb5543ff84e76031cd3138af2515bfba49131b
Yangdaehan <yangdh08@naver.com>
2024-06-27 01:43:40 +0900

    Feat: 감사 일기에 대한 칭찬 기능 추가
    사용자가 적은 감사 일기에 대해 칭찬이 자동으로 출력되게 기능 추가.

--- src/main/java/org/sopt/gptapi/service/ChatService.java
+++ src/main/java/org/sopt/gptapi/service/ChatService.java
@@ -15,7 +15,10 @@ public class ChatService {
 
     public String getChatResponse(String content) {
         try {
-            return chatgptService.sendMessage(content);
+
+            String message = "[" + content + "] 오늘 있었던 감사한 일에 대해 작성했어. 이를 보고 칭찬해줘.";
+
+            return chatgptService.sendMessage(message);
         } catch (HttpClientErrorException.BadRequest e) {
             return ErrorMessage.BAD_REQUEST.getMessage();
         } catch (Exception e) {
