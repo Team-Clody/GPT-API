1828bb3270d392424eb33d618213214cf64cafb9
Yangdaehan <yangdh08@naver.com>
2024-07-09 11:08:53 +0900

    욕설 및 비속어 필터링 기능 추가

--- src/main/java/org/sopt/gptapi/service/ChatService.java
+++ src/main/java/org/sopt/gptapi/service/ChatService.java
@@ -1,6 +1,9 @@
 package org.sopt.gptapi.service;
 
+import com.vane.badwordfiltering.BadWordFiltering;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sopt.gptapi.common.dto.ErrorMessage;
 import org.sopt.gptapi.common.dto.Prompt;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,26 @@ public class ChatService {
 
     private final AsyncChatgptService asyncChatgptService;
 
+    BadWordFiltering filtering = new BadWordFiltering();
+    private static final Logger logger = LoggerFactory.getLogger(ChatService.class);
+
     public Mono<String> getChatResponse(String content) {
-        String message = Prompt.MESSAGE.getMessage() + content + "\n칭찬 :";
-        return asyncChatgptService.sendMessage(message)
-            .onErrorResume(HttpClientErrorException.BadRequest.class, e -> Mono.just(ErrorMessage.BAD_REQUEST.getMessage()))
-            .onErrorResume(e -> Mono.just(ErrorMessage.GENERAL_ERROR.getMessage()));
+
+        boolean bool1 = filtering.check(content);
+
+        if (bool1) {
+            return Mono.just(content);
+        } else {
+            String message = Prompt.MESSAGE.getMessage() + content + "\n칭찬 :";
+            return asyncChatgptService.sendMessage(message)
+                    .onErrorResume(HttpClientErrorException.BadRequest.class, e -> {
+                        logger.error("BadRequest error: {}", ErrorMessage.BAD_REQUEST.getMessage(), e);
+                        return Mono.just(ErrorMessage.BAD_REQUEST.getMessage());
+                    })
+                    .onErrorResume(e -> {
+                        logger.error("General error: {}", ErrorMessage.GENERAL_ERROR.getMessage(), e);
+                        return Mono.just(ErrorMessage.GENERAL_ERROR.getMessage());
+                    });
+        }
     }
 }
